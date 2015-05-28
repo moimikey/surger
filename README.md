01fdecfa5b5167e65e44704c3f999ad07419001f
Michael Scott Hertzberg <mshertzberg@gmail.com>
2015-05-28 17:26:38 -0400

    add location details as a reference + refactor

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # surger
+[![Version][npm-version-image]][npm-version-url] [![License][npm-license-image]][npm-license-url] [![Downloads][npm-downloads-image]][npm-downloads-url] [![Deps][npm-deps-image]][npm-deps-url] [![DevDeps][npm-devdeps-image]][npm-devdeps-url] [![Build Status][drone-build-image]][drone-build-url]
+
+## about
 Is there surge pricing around me right now? This little CLI utility will determine your location (currently OS X only) using location services and determine if surge pricing is active within your range.
 
 ## installation
 ```
 $ npm -g install surger
 $ surger
+
+Location:  Nearby East 2nd Street
+
 uberX       ✔ GOOD
 uberXL      ✔ GOOD
 UberBLACK   $ SURGE 2.8x
 UberSUV     $ SURGE 2.8x
 uberT       ✔ GOOD
 ```
+
+[npm-version-url]: https://www.npmjs.com/package/surger
+[npm-version-image]: https://img.shields.io/npm/v/surger.svg
+[npm-license-url]: https://github.com/moimikey/shitty-uber/blob/master/LICENSE
+[npm-license-image]: https://img.shields.io/npm/l/surger.svg
+[npm-downloads-url]: https://www.npmjs.com/package/surger
+[npm-downloads-image]: https://img.shields.io/npm/dm/surger.svg
+[npm-deps-url]: https://david-dm.org/moimikey/surger
+[npm-deps-image]: https://img.shields.io/david/moimikey/surger.svg
+[npm-devdeps-url]: https://david-dm.org/moimikey/surger
+[npm-devdeps-image]: https://img.shields.io/david/dev/moimikey/surger.svg
+[drone-build-image]: https://drone.io/github.com/moimikey/shitty-uber/status.png
+[drone-build-url]: https://drone.io/github.com/moimikey/shitty-uber/latest
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const chalk = require('chalk');
 const table = require('text-table');
 const gps   = require('wifi-location');
+const geo   = require('node-geocoder')('google', 'http');
 const pkg   = require('../package');
 const uber  = require('uber-api')(pkg.uber);
 const yay   = chalk.dim.bgGreen.bold;
@@ -14,26 +15,54 @@ export default class Surge {
     this.getCoords(this.handleCoords.bind(this));
   }
 
+  isNonEmptyObject(obj) {
+    return Object.getOwnPropertyNames(obj || {}).length !== 0
+  }
+
+  flatten(arr) {
+    return arr.reduce((i, chunk) => {
+      return i.concat(chunk);
+    });
+  }
+
   handleCoords(err, data) {
-    if (err || typeof data !== 'object') throw Error(err);
+    if (err) throw (err);
     let { lat, lng } = data;
+    this.getDetailsFromLatLng(this.handleDetails.bind(this), lat, lng);
     this.getPriceEstimateFromLatLng(this.handlePriceEstimate.bind(this), lat, lng);
   }
 
   handlePriceEstimate(err, estimates) {
-    if (err) throw Error(err);
+    if (err) throw (err);
     this.parseEstimates(estimates);
   }
 
+  handleDetails(err, details) {
+    if (err) throw (err);
+    this.parseDetails(this.flatten(details));
+  }
+
   parseEstimates(estimates) {
-    if (!estimates) throw Error('Cannot get estimates :(...');
     console.log(table(estimates));
   }
 
+  parseDetails(details) {
+    console.log('');
+    console.log('Location:  Nearby', chalk.dim.blue(details.streetName));
+    console.log('');
+  }
+
+  getDetailsFromLatLng(cb, lat, lng) {
+    geo.reverse({ lat: lat, lon: lng }, ((err, res) => {
+      if (err) cb(err);
+      cb(null, res);
+    }));
+  }
+
   getPriceEstimateFromLatLng(cb, lat, lng) {
-    uber.getPriceEstimate(lat, lng, lat, lng, (err, response) => {
-      if (err || typeof response !== 'object') return cb(err);
-      this.estimates = response.prices.map((type) => {
+    uber.getPriceEstimate(lat, lng, lat, lng, (err, resp) => {
+      if (err) return cb(err);
+      this.estimates = resp.prices.map((type) => {
         let good = yay(' ✔ GOOD ');
         let bad  = nay(' $ SURGE', chalk.white.bgRed(type.surge_multiplier + 'x '));
         return [
@@ -47,10 +76,11 @@ export default class Surge {
 
   getCoords(cb) {
     gps.getTowers((err, towers) => {
-      if (err || typeof towers !== 'object') return cb(err);
-      gps.getLocation(towers, (err, loc) => {
-        if (err || typeof loc !== 'object') return cb(err);
-        let { latitude, longitude } = loc;
+      if (err) return cb(err);
+      gps.getLocation(towers, (err, location) => {
+        if (err) return cb(err);
+        if (!this.isNonEmptyObject(location)) return cb(Error(chalk.dim.red('Briefly unable to locate you. Try again.')));
+        let { latitude, longitude } = location;
         let coords = { lat: latitude, lng: longitude };
         cb(null, coords);
       });
